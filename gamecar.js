671caac8067665e035054f609168e3bd8c4e013b
ĐỨC MẠNH <ducmanh27101994@gmail.com>
2020-06-14 12:53:56 +0700

    tạo hàm di chuyển trái phải cho car

--- gamecar.js
+++ gamecar.js
@@ -21,7 +21,23 @@ function drawRect(x, y, width, height, color) {
     ctx.fillRect(x, y, width, height)
 }
 
+document.addEventListener("keydown",moveCar);
+let move;
+function moveCar(evt) {
+    let key = evt.keyCode;
+    if (key === 37) {
+        move = "LEFT";
+    } else if (key === 39) {
+        move = "RIGHT";
+    }
+}
+
+
+
 function update() {
+    drawRect(car.x, car.y, car.width, car.height, car.color);
+}
 
-
+function start() {
+    game = setInterval(update,100);
 }
